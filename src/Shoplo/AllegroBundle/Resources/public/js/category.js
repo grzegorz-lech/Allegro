5198470476e4fd1a8b21205fbb01c9dbf3bba4b5
Grzegorz Lech <lech.grzegorz@gmail.com>
2013-01-07 10:45:39 +0100

    automatyczne mapowanie podkategorii z kategorii glownej

--- src/Shoplo/AllegroBundle/Resources/public/js/category.js
+++ src/Shoplo/AllegroBundle/Resources/public/js/category.js
@@ -6,7 +6,7 @@ $(function(){
     $('.map-allegro').submit(validateForm);
 });
 
-function selectCategory(handler)
+function selectCategory(handler, automatic)
 {
     var category_id = $(handler).find('option:selected').val();
     var last_child = !!(typeof($(handler).find('option:selected').attr('data-childs')) != 'undefined' && $(handler).find('option:selected').attr('data-childs') == 0);
@@ -14,6 +14,30 @@ function selectCategory(handler)
     {
         $(handler).parents('.control-group').find('input.map').val(category_id);
     }
+
+
+    if ( typeof(automatic) == 'undefined' )
+    {
+        $(handler).attr('class', $(handler).attr('class')+' changed');
+    }
+
+
+    var column = $(handler).parents('.control-group').find('select').index( $(handler) );
+    var mainParent = $(handler).parents('.control-group').find('select:first');
+    $('.control-group').each(function(){
+        if ( $(this).find('select').length )
+        {
+            var parent = $(this).find('select:first');
+            var select = $(this).find('select')[column];
+            if ( parent.data('parent') == mainParent.data('id') && (parent.val() == 0 || !parent.hasClass('changed')) )
+            {
+                $(select).val($(handler).find('option:selected').val());
+                selectCategory($(select), true);
+            }
+        }
+    });
+
+
 
     if ( category_id == 0 || (typeof($(handler).find('option:selected').attr('data-childs')) != 'undefined' && $(handler).find('option:selected').attr('data-childs') == 0) )
     {
@@ -28,6 +52,7 @@ function selectCategory(handler)
             $(handler).nextUntil().remove();
             if ( data.length > 0 )
             {
+                var parent_id = $(handler).data('parent') == 0 ? $(handler).data('id') : $(handler).data('parent');
                 var select_id = $(handler).attr('id')+'-'+category_id;
                 var html = $('<select id="'+select_id+'" class="allegro span2" name="'+$(handler).attr('name')+'"></select>');
 
